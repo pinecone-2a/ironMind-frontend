118cab7f03dc99f62591f350356bb6cf241586e0
24LP8321 24LP8321 <24LP8321@G302-CC0181.local>
2025-02-04 17:58:30 +0800

    update

--- src/app/(view-page)/layout.tsx
+++ src/app/(view-page)/layout.tsx
@@ -5,11 +5,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
-      </body>
-    </html>
+ <> 
+ {children}
+ </>
+       
+    
   );
 }
