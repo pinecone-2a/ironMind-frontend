58f3307513bfb1e020df8681c57de90500090d60
24LP8321 24LP8321 <24LP8321@G302-CC0181.local>
2025-02-05 14:40:22 +0800

    update

--- src/app/(view-page)/explore/page.tsx
+++ src/app/(view-page)/explore/page.tsx
@@ -1,19 +1,32 @@
 export default function Home(){
     return<>
-    <div className="w-[957px] h-[880px] p-5">
+    <div className="w-[957px] h-[880px] p-5 ml-auto mr-auto">
         <h1 className="font-semibold text-[20px] mb-5">Explore creators</h1>
-        <button className="h-[36px] w-[243px] border rounded-md mb-5"></button>
+        <input className="h-[36px] w-[243px] border rounded-md mb-5 text-[14px] px-4" placeholder="Seach name"></input>
         <div className="w-[909px] h-[224px] border rounded-md p-5">
             <div className="flex justify-between">
               <div className="flex gap-3 items-center">
                 <div className="w-[40px] h-[40px] border rounded-full"></div>
-                <h1 className="font-semibold text-[20px]">Name of creator</h1>
+                <h1 className="font-semibold text-[20px]">Name of the creator</h1>
               </div>
               <button className="bg-[#F4F4F5] w-[136px] h-[40px] rounded-md">View profile</button>
             </div>
 
-            <div></div>
+            <div className="flex justify-between mt-4">
+              <div className="flex flex-col w-[420px] gap-3">
+                <p className="font-semibold">About the creator</p>
+                <p className="text-[14px]">All day, every day, we're watching, listening to, reading and absorbing politics. It's exhausting. We then report on what we've seen in a way that's as chill as possible. None of the sensationalism and division you'll find elsewhere</p>
+              </div>
+
+              <div className="flex flex-col w-[420px] gap-3">
+                <p className="font-semibold">Social media URL</p>
+                <p className="text-[14px]">The URL should be here</p>
+              </div>
+            </div>
         </div>
+        <div>hello</div>
+        <div>heilll</div>
+        <div>bfisbifgsifgiefgsi</div>
     </div></>
     
 }
