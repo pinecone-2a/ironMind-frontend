efdb18de727642de0563bc1e68e2d5af9121bf79
Gunjet <gunjegunje54@gmail.com>
2025-02-06 01:49:40 +0800

    update

--- src/app/log-in/_components/form.tsx
+++ src/app/log-in/_components/form.tsx
@@ -11,24 +11,39 @@ import { Form,
   FormLabel,
   FormMessage, } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 const formSchema = z.object({
-  username: z.string().min(5, {
-    message: "Username must be at least 5 characters.",
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
   }),
+  email: z.string().email({
+    message: "Please enter a valid email"
+  }),
+  password: z
+  .string()
+  .min(6,{
+    message: "Password must be 6 or more characters long" 
+  })
 })
 
-export function ProfileForm1() {
+export function ProfileForm1({ onSubmit }: { onSubmit: (username: string) => void }) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
   })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
+  const handleClick = () => {
+    const username = form.getValues("username")
+    if (username.length >= 2) {
+      onSubmit(username)
+    } else {
+      form.setError("username", {
+        message: "Username must be at least 2 characters",
+      })
+    }
+  };
 
       return(
         <div className="h-screen w-[40%] justify-center items-center flex relative">
@@ -36,7 +51,7 @@ export function ProfileForm1() {
          <p className="font-semibold text-[24px] mb-2">Create your account</p>
          <p className="text-[#71717A] text-[14px] mb-4">Choose a username for your page</p>
          <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <form className="space-y-8 w-[140%]">
            <FormField
              control={form.control}
              name="username"
@@ -46,11 +61,11 @@ export function ProfileForm1() {
                  <FormControl>
                    <Input placeholder="Enter username here" {...field} />
                  </FormControl>
-                 <FormMessage />
+                <FormMessage></FormMessage>
                </FormItem>
               )}
             />
-            <Button>Continue</Button>
+            <Button className="w-[100%]" onClick={handleClick}>Continue</Button>
            </form>
           </Form>
          </div>
@@ -59,43 +74,53 @@ export function ProfileForm1() {
       )
 }
 
-export function ProfileForm2() {
+export function ProfileForm2({ username }: { username: string }) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      username: username, email: "", password: "" 
     },
   })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
+
+
+const handleSubmit = (data: z.infer<typeof formSchema>) => {
+  console.log("Form Data", data); // You can handle form submission here
+};
 
   return(
     <div className="h-screen w-[40%] justify-center items-center flex relative">
     <div className=" flex flex-col items-start">
-     <p className="font-semibold text-[24px] mb-2">Welcome, baconpancakes1</p>
+     <p className="font-semibold text-[24px] mb-2">Welcome,  {username}</p>
      <p className="text-[#71717A] text-[14px] mb-4">Connect email and set a password</p>
      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8 w-[140%]">
        <FormField
          control={form.control}
-         name="username"
+         name="email"
          render={({ field }) => (
            <FormItem>
              <FormLabel>Email</FormLabel>
              <FormControl>
                <Input placeholder="Enter email here" {...field} />
              </FormControl>
-
+             <FormMessage/>
+           </FormItem>
+          )}
+        />
+       <FormField
+         control={form.control}
+         name="password"
+         render={({ field }) => (
+           <FormItem>
              <FormLabel>Password</FormLabel>
              <FormControl>
                <Input placeholder="Enter password here" {...field} />
              </FormControl>
+             <FormMessage/>
            </FormItem>
           )}
         />
-        <Button>Continue</Button>
+        <Button className="w-[100%]">Continue</Button>
        </form>
       </Form>
      </div>
@@ -104,47 +129,4 @@ export function ProfileForm2() {
   )
 }
 
-export function ProfileForm3() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
 
-  return(
-    <div className="h-screen w-[40%] justify-center items-center flex relative">
-    <div className=" flex flex-col items-start">
-     <p className="font-semibold text-[24px] mb-2">Welcome, baconpancakes1</p>
-     <p className="text-[#71717A] text-[14px] mb-4">Connect email and set a password</p>
-     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-       <FormField
-         control={form.control}
-         name="username"
-         render={({ field }) => (
-           <FormItem>
-             <FormLabel>Email</FormLabel>
-             <FormControl>
-               <Input placeholder="Enter email here" {...field} />
-             </FormControl>
-
-             <FormLabel>Password</FormLabel>
-             <FormControl>
-               <Input placeholder="Enter password here" {...field} />
-             </FormControl>
-           </FormItem>
-          )}
-        />
-        <Button>Continue</Button>
-       </form>
-      </Form>
-     </div>
-     <button className="bg-[#F4F4F5] px-5 py-3 rounded-md text-[14px] absolute top-7 right-0">Log In</button>
-    </div>
-  )
-}
--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -1,33 +1,22 @@
-// export default function Page() {
-//     return(
-//         <div className="flex gap-8 justify-center">
-//             <img src="https://res.cloudinary.com/dht5mewgk/image/upload/v1738738837/kogl1awioe0xhgmtnj6y.png" className="h-screen"></img>
-//             <div className="h-screen w-[40%] justify-center items-center flex relative">
+"use client"
 
-//                 <div className=" flex flex-col items-start">
-//                   <p className="font-semibold text-[24px] mb-2">Create your account</p>
-//                   <p className="text-[#71717A] text-[14px] mb-4">Choose a username for your page</p>
-//                   <p className="text-[14px] mb-[2px]">Username</p>
-//                   <input className="rounded-md border p-2 px-3 w-[359px] text-[14px] mb-6" placeholder="Enter username here"></input>
-//                   <button className="bg-[#E4E4E7] rounded-md p-2 w-[359px] text-white">Continue</button>
-//                 </div>
-
-//                 <button className="bg-[#F4F4F5] px-5 py-3 rounded-md text-[14px] absolute top-7 right-0">Log In</button>
-
-//             </div>
-//         </div>
-//     )
-// }
-
-
-import { ProfileForm1 } from "./_components/forms"; 
+import { useState } from "react";
+import { ProfileForm1, ProfileForm2 } from "./_components/form"; 
 
 export default function Form() {
+  const [page, setPage] = useState(1)
+  const [username, setUsername] = useState("")
+  const handleForm1Submit = (username: string) =>{
+    setUsername(username)
+    setPage(2)
+  }
+
 
   return (
     <div className="flex gap-8 justify-center">
-    <img src="https://res.cloudinary.com/dht5mewgk/image/upload/v1738738837/kogl1awioe0xhgmtnj6y.png" className="h-screen"></img>
-     <ProfileForm1/>
+      <img src="https://res.cloudinary.com/dht5mewgk/image/upload/v1738738837/kogl1awioe0xhgmtnj6y.png" className="h-screen"></img>
+      {page === 1 && (<ProfileForm1 onSubmit={handleForm1Submit}/>)}
+      {page === 2 && (<ProfileForm2 username={username}/>)}
     </div>
   )
 }
