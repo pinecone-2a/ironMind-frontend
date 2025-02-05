2e8000727ab5b3a3ca2d6f43d825b494778b6d52
24LP8321 24LP8321 <24LP8321@G302-CC0181.local>
2025-02-05 20:24:56 +0800

    update

---
+++ src/app/log-in/_components/forms.tsx
@@ -0,0 +1,105 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage, } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(5, {
+    message: "Username must be at least 5 characters.",
+  }),
+})
+
+export function ProfileForm1() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+      return(
+        <div className="h-screen w-[40%] justify-center items-center flex relative">
+        <div className=" flex flex-col items-start">
+         <p className="font-semibold text-[24px] mb-2">Create your account</p>
+         <p className="text-[#71717A] text-[14px] mb-4">Choose a username for your page</p>
+         <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+           <FormField
+             control={form.control}
+             name="username"
+             render={({ field }) => (
+               <FormItem>
+                 <FormLabel>Username</FormLabel>
+                 <FormControl>
+                   <Input placeholder="Enter username here" {...field} />
+                 </FormControl>
+                 <FormMessage />
+               </FormItem>
+              )}
+            />
+            <Button>Continue</Button>
+           </form>
+          </Form>
+         </div>
+         <button className="bg-[#F4F4F5] px-5 py-3 rounded-md text-[14px] absolute top-7 right-0">Log In</button>
+        </div>
+      )
+}
+
+export function ProfileForm2() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return(
+    <div className="h-screen w-[40%] justify-center items-center flex relative">
+    <div className=" flex flex-col items-start">
+     <p className="font-semibold text-[24px] mb-2">Welcome, baconpancakes1</p>
+     <p className="text-[#71717A] text-[14px] mb-4">Connect email and set a password</p>
+     <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+       <FormField
+         control={form.control}
+         name="username"
+         render={({ field }) => (
+           <FormItem>
+             <FormLabel>Email</FormLabel>
+             <FormControl>
+               <Input placeholder="Enter email here" {...field} />
+             </FormControl>
+
+             <FormLabel>Password</FormLabel>
+             <FormControl>
+               <Input placeholder="Enter password here" {...field} />
+             </FormControl>
+           </FormItem>
+          )}
+        />
+        <Button>Continue</Button>
+       </form>
+      </Form>
+     </div>
+     <button className="bg-[#F4F4F5] px-5 py-3 rounded-md text-[14px] absolute top-7 right-0">Log In</button>
+    </div>
+  )
+}
--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -19,69 +19,15 @@
 //     )
 // }
 
-"use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { ProfileForm1 } from "./_components/forms"; 
 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-  username: z.string().min(5, {
-    message: "Username must be at least 5 characters.",
-  }),
-})
-
-export default function ProfileForm() {
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
+export default function Form() {
 
   return (
     <div className="flex gap-8 justify-center">
     <img src="https://res.cloudinary.com/dht5mewgk/image/upload/v1738738837/kogl1awioe0xhgmtnj6y.png" className="h-screen"></img>
-    <div className="h-screen w-[40%] justify-center items-center flex relative">
-     <div className=" flex flex-col items-start">
-      <p className="font-semibold text-[24px] mb-2">Create your account</p>
-      <p className="text-[#71717A] text-[14px] mb-4">Choose a username for your page</p>
-      <Form {...form}>
-       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter username here" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           )}
-         />
-         <Button>Continue</Button>
-        </form>
-       </Form>
-      </div>
-     </div>
+     <ProfileForm1/>
     </div>
   )
 }
