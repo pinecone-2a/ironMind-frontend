8602e9663c5cbe6a93f8fdac5de9e094a4365b2c
Chinguun19 <centillionc0919@gmail.com>
2025-02-06 12:57:45 +0800

    update

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -1,46 +1,119 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { CreateProfileStep1 } from "./stepOne"
-import { CreateProfileStep2 } from "./stepTwo"
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import { CreateProfileStep1 } from "./stepOne";
+import { CreateProfileStep2 } from "./stepTwo";
+import { useState, ChangeEvent } from "react";
+
+
+export type UserInfo = {
+  image: string;
+  name: string;
+  about: string;
+  socialURL: string;
+  country: string;
+  firstName: string;
+  lastName: string;
+  cardNumber: string;
+  expires: string;
+  year: string;
+  cvc: string;
+};
+
+
+type ErrorInfo = {
+  image: string;
+  name: string;
+  about: string;
+  socialURL: string;
+  country: string;
+  firstName: string;
+  lastName: string;
+  cardNumber: string;
+  expires: string;
+  year: string;
+  cvc: string;
+};
 
 export default function CreateProfile() {
-const [currentStep, SetCurrentStep] = useState(1)
-const [userInfo, setUserInfo] = useState({
-image: "",
-name: "",
-about: "",
-socialURL: ""
+  const [currentStep, setCurrentStep] = useState<number>(2);
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    image: "",
+    name: "",
+    about: "",
+    socialURL: "",
+    country: "",
+    firstName: "",
+    lastName: "",
+    cardNumber: "",
+    expires: "",
+    year: "2025",
+    cvc: "",
+  });
 
-})
+  const [error, setError] = useState<ErrorInfo>({
+    image: "",
+    name: "",
+    about: "",
+    socialURL: "",
+    country: "",
+    firstName: "",
+    lastName: "",
+    cardNumber: "",
+    expires: "",
+    year: "",
+    cvc: "",
+  });
 
-const [error, setError] = useState({
-image: "",
-name: "",
-about: "",
-socialURL: ""
-})
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { id, value } = e.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
 
-const onChange = (e: any) => {
-const field = e.target.id;
-const value = e.target.value;
-const newValues = {...userInfo, [field]: value};
+    setError((prev) => ({
+      ...prev,
+      [id]: "",
+    }));
+  };
 
-}
+  const validateFields = () => {
+    const newErrors: ErrorInfo = {
+      image: userInfo.image ? "" : "Please enter image",
+      name: userInfo.name ? "" : "Please enter name",
+      about: userInfo.about ? "" : "Please enter info about yourself",
+      socialURL: userInfo.socialURL ? "" : "Please enter a social link",
+      country: userInfo.country ? "" : "Select country to continue",
+      firstName: userInfo.firstName ? "" : "First name must match",
+      lastName: userInfo.lastName ? "" : "Last name must match",
+      cardNumber: userInfo.cardNumber ? "" : "Invalid card number",
+      expires: userInfo.expires ? "" : "Invalid month",
+      year: userInfo.year ? "" : "Invalid month",
+      cvc: userInfo.cvc ? "" : "Invalid month",
+    };
 
+    setError(newErrors);
+    return Object.values(newErrors).every((err) => err === ""); 
+  };
 
+  const handleNext = () => {
+    if (validateFields()) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
 
+  const handleSubmit = () => {
+    if (validateFields()) {
+      console.log("Submit Data:", userInfo);
+    }
+  };
 
+  return (
+    <> 
+      {currentStep === 1 && <CreateProfileStep1 userInfo={userInfo} error={error} onChange={onChange}  handleNext={handleNext}/>}
+      {currentStep === 2 && <CreateProfileStep2 userInfo={userInfo} error={error} onChange={onChange} handleSubmit={handleSubmit} />}
 
-return (
-<div>
-  <CreateProfileStep1/>
-
-
-
-</div>
-
-)
+     </>
+  );
 }
